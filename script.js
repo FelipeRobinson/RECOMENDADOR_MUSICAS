b2e757b47e3f76384ea90f0a641aef66d8683da6
Felipe Spider <feliperobinsonprofissional@gmail.com>
2024-06-14 11:58:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 var Idade;
-var Fantasia;
-var Aventura;
-var Acao;
-var Drama;
-var Terror;
+var Trap;
+var Rap;
+var PopInternacional;
+var PopNacional;
+var Pagode;
+var Funk;
+var Rock;
+var Sertanejo;
 
 function setup() {
   createCanvas(800, 400);
   createElement("h2", "Recomendador de filmes");
   createSpan("Sua idade:");
   Idade = createInput("5");
-  Fantasia = createCheckbox("Gosta de fantasia?");
-  Aventura = createCheckbox("Gosta de aventura?");
-  Acao = createCheckbox("Gosta de ação?");
-  Drama = createCheckbox("Gosta de drama?");
+  Trap = createCheckbox("Gosta de Trap?");
+  Rap = createCheckbox("Gosta de Rap?");
+  PopInternacional = createCheckbox("Gosta de Pop Internacional?");
+  PopNacional = createCheckbox("Gosta de Pop Nacional?");
+  Pagode = createCheckbox("Gosta de Pagode?");
+  Funk = createCheckbox("Gosta de Funk?");
+  Rock = createCheckbox("Gosta de Rock?");
+  Sertanejo = createCheckbox("Gosta de Sertanejo?");
 }
 
 function draw() {
   background("white");
   var idade = Idade.value();
-  var gostaDeFantasia = Fantasia.checked();
-  var gostaDeAventura = Aventura.checked();
-  var gostaDeAcao = Acao.checked();
-  var gostaDeDrama = Drama.checked();
-  var recomenda = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura, gostaDeAcao, gostaDeDrama);
+  var gostaDeTrap = Trap.checked();
+  var gostaDeRap = Rap.checked();
+  var gostaDePopInternacional = PopInternacional.checked();
+  var gostaDePopNacional = PopNacional.checked();
+  var gostaDePagode = Pagode.checked();
+  var gostaDeFunk = Funk.checked();
+  var gostaDeRock = Rock.checked();
+  var gostaDeSertanejo = Sertanejo.checked();
+  var recomenda = geraRecomendacao(idade, gostaDeTrap, gostaDeRap, gostaDePopInternacional, gostaDePopNacional, gostaDePagode, gostaDeFunk, gostaDeRock, gostaDeSertanejo);
 
   fill (color(76, 0, 115));
   textAlign (CENTER, CENTER);
@@ -31,53 +42,137 @@ function draw() {
   text (recomenda, width / 2, height / 2);
 }
 
-function geraRecomendacao (idade, gostaDeFantasia, gostaDeAventura, gostaDeAcao, gostaDeDrama) {
-  if (idade >= 10) {
-    if (idade >= 14) {
-      return "O menino que descobriu o vento";
-    }
-    if (idade >=12) { 
-      if (idade >= 12) {
-          if (gostaDeFantasia && gostaDeAventura) {
-            return "Homem aranha: no aranhaverso";          
-          } 
-          else{
-          return "Ladrões de bicicleta";
-          }
-        } 
-        if (gostaDeDrama) {
-          return "Romeu e Julieta";
-        }
-        else {
-          if (gostaDeFantasia) {
-            return "As aventuras de pi";
-          } 
-          else {
-            return "Depois da chuva";
-          }
-        }
+function geraRecomendacao (idade, gostaDeTrap, gostaDeRap, gostaDePopInternacional, gostaDePopNacional, gostaDePagode, gostaDeFunk, gostaDeRock, gostaDeSertanejo) {
+  if (idade <= 18) {
+    if (idade <= 12) {
+      if (gostaDeTrap) {
+        return "Atos 2 (Trinade Records)";
+      }
+      if (gostaDeRap) {
+        return "Espere mais um tempo...";
+      }
+      if (gostaDePopInternacional) {
+        return "Billy Jeans (Michael Jackson)";
+      }
+      if (gostaDePopNacional) {
+        return "Meu Abrigo (Melim)";
+      }
+      if (gostaDePagode) {
+        return "Deixa acontecer naturalmente (Grupo Revelação)";
+      }
+      if (gostaDeFunk) {
+        return "Espere mais um pouco...";
+      }
+      if (gostaDeRock) {
+        return "Admiravel ship novo (Pitty)";
+      }
+      if (gostaDeSertanejo) {
+        return "Menino da Porteira (Daniel)";
       }
     }
-    if (idade >= 18) {
-      if (gostaDeDrama) {
-        return "50 tons de cinza"
+    if (idade <= 14) {
+      if (gostaDeTrap) {
+        return "Poesia Acústica 14 (Pinepple Storm)";
       }
-      if (gostaDeFantasia) {
-        return ""
+      if (gostaDeRap) {
+        return "Ela só quer paz (Projota)";
       }
-      if (gostaDeAventura) {
-        return ""
+      if (gostaDePopInternacional) {
+        return "Shake It Of (Taylor Swift)";
+      }
+      if (gostaDePopNacional) {
+        return "";
+      }
+      if (gostaDePagode) {
+        return "Deixa acontecer naturalmente (Grupo Revelação)";
+      }
+      if (gostaDeFunk) {
+        return "Espere mais um pouco...";
+      }
+      if (gostaDeRock) {
+        return "Admiravel ship novo (Pitty)";
+      }
+      if (gostaDeSertanejo) {
+        return "Menino da Porteira (Daniel)";
       }
     }
-    else {
-      if (gostaDeFantasia) {
-        return "A viagem de chihiro";
-      } 
-      if (gostaDeAventura) {
-        return "O feitiço do tempo";
+    if (idade <= 16) {
+      if (gostaDeTrap) {
+        return "Atos 2 (Trinade Records)";
       }
-      if (gostaDeAcao) {
-        return "Power Rangers";
-      } 
+      if (gostaDeRap) {
+        return "Espere mais um tempo...";
+      }
+      if (gostaDePopInternacional) {
+        return "Billy Jeans (Michael Jackson)";
+      }
+      if (gostaDePopNacional) {
+        return "Meu Abrigo (Melim)";
+      }
+      if (gostaDePagode) {
+        return "Deixa acontecer naturalmente (Grupo Revelação)";
+      }
+      if (gostaDeFunk) {
+        return "Espere mais um pouco...";
+      }
+      if (gostaDeRock) {
+        return "Admiravel ship novo (Pitty)";
+      }
+      if (gostaDeSertanejo) {
+        return "Menino da Porteira (Daniel)";
+      }
     }
+    if (idade <= 18) {
+      if (gostaDeTrap) {
+        return "Atos 2 (Trinade Records)";
+      }
+      if (gostaDeRap) {
+        return "Espere mais um tempo...";
+      }
+      if (gostaDePopInternacional) {
+        return "Billy Jeans (Michael Jackson)";
+      }
+      if (gostaDePopNacional) {
+        return "Meu Abrigo (Melim)";
+      }
+      if (gostaDePagode) {
+        return "Deixa acontecer naturalmente (Grupo Revelação)";
+      }
+      if (gostaDeFunk) {
+        return "Espere mais um pouco...";
+      }
+      if (gostaDeRock) {
+        return "Admiravel ship novo (Pitty)";
+      }
+      if (gostaDeSertanejo) {
+        return "Menino da Porteira (Daniel)";
+      }
+    }
+  }
+  if (idade >= 18) {
+    if (gostaDeTrap) {
+      return "Atos 2 (Trinade Records)";
+    }
+    if (gostaDeRap) {
+      return "Espere mais um tempo...";
+    }
+    if (gostaDePopInternacional) {
+      return "Billy Jeans (Michael Jackson)";
+    }
+    if (gostaDePopNacional) {
+      return "Meu Abrigo (Melim)";
+    }
+    if (gostaDePagode) {
+      return "Deixa acontecer naturalmente (Grupo Revelação)";
+    }
+    if (gostaDeFunk) {
+      return "Espere mais um pouco...";
+    }
+    if (gostaDeRock) {
+      return "Admiravel ship novo (Pitty)";
+    }
+    if (gostaDeSertanejo) {
+      return "Menino da Porteira (Daniel)";
+    }
+  }
 }
