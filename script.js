19047d3161bfb4581d200f5aa1d1b7bbe8958978
Felipe Spider <feliprerobinsonprofissional@gmail.com>
2024-06-22 17:06:01 -0300

    Update

--- script.js
+++ script.js
@@ -1,125 +1,82 @@
 var Idade;
-var Rap;
-var Trap;
-var PopInter;
-var PopNac;
-var Sertanejo;
-var Rock;
-var MPB;
+var Rap, Trap, PopInter, PopNac, Rock;
 
-var currentSong;
+var beggin, diario, clickbait, talisma, desejo, vafalume, setevias;
+
+var currentSong
+
+function preload() {
+  beggin = loadSound("beggin.mp3")
+  clickbait = loadSound("clickbait.mp3")
+  talisma = loadSound("talisma.mp3")
+  desejo = loadSound("desejo.mp3")
+  vafalume = loadSound("vafalume.mp3")
+  setevias = loadSound("setevidas.mp3")
+}
 
 function setup() {
   createCanvas(800, 400);
   createElement("h1", "Recomendador de Músicas");
+  var aviso = createElement("h3", "Aviso: não selecione mais de uma opção.");
+  aviso.style("color", "red");
+  
   createSpan("Sua idade:");
   Idade = createInput("5");
-  Rap = createCheckbox("Gosta de Rap?");
   Trap = createCheckbox("Gosta de Trap?");
-  PopInter = createCheckbox("Gosta de Pop Internacional?");
   PopNac = createCheckbox("Gosta de Pop Nacional?");
-  Sertanejo = createCheckbox("Gosta de Sertanejo?");
   Rock = createCheckbox("Gosta de Rock?");
-  MPB = createCheckbox("Gosta de MPB?");
 }
 
 function draw() {
   background("black");
   var idade = Idade.value();
-  var gostaDeRap = Rap.checked();
   var gostaDeTrap = Trap.checked();
-  var gostaDePopInter = PopInter.checked();
   var gostaDePopNac = PopNac.checked();
-  var gostaDeSertanejo = Sertanejo.checked();
   var gostaDeRock = Rock.checked();
-  var gostaDeMPB = MPB.checked();
   
-  var songRecomendacao = geraRecomendacao(idade, gostaDeRap, gostaDeTrap, gostaDePopInter, gostaDePopNac, gostaDeSertanejo, gostaDeRock, gostaDeMPB);
-
-  if (songRecomendacao !== currentSong) {
+  var Musica = TocandoMusica(idade, gostaDeTrap, gostaDePopNac, gostaDeRock);
+  
+  if (Musica !== currentSong) {
     if (currentSong) {
       currentSong.stop();
     }
-    loadSound(songRecomendacao.file, function(sound) {
-      sound.play();
-      currentSong = sound;
-    });
+    if (Musica) {
+      Musica.play();
+    }
+    currentSong = Musica;
   }
+
+  var recomenda = geraRecomendacao(idade, gostaDeTrap, gostaDePopNac, gostaDeRock);
 
   fill(color("red"));
   textAlign(CENTER, CENTER);
-  textSize(38);
-  text(songRecomendacao.name, width / 2, height / 2);
+  textSize(60);
+  text(recomenda, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade, gostaDeRap, gostaDeTrap, gostaDePopInter, gostaDePopNac, gostaDeSertanejo, gostaDeRock, gostaDeMPB) {
-  if (idade <= 18) {
-    if (idade <= 10) {
-      if (gostaDeRap) {
-        return { name: "Desabafo/Deixa eu dizer (Marcelo D2)", file: "assets/desabafo.mp3" };
-      }
-      if (gostaDeTrap) {
-        return { name: "Atos 2 (Trindade Records)", file: "assets/atos2.mp3" };
-      }
-      if (gostaDePopInter) {
-        return { name: "Granade (Bruno Mars)", file: "assets/granade.mp3" };
-      }
-      if (gostaDePopNac) {
-        return { name: "Anna Júlia (Los Hermanos)", file: "assets/anajulia.mp3" };
-      }
-      if (gostaDeSertanejo) {
-        return { name: "Menino da Porteira (Daniel)", file: "assets/meninoDaPorteira.mp3" };
-      }
-      if (gostaDeRock) {
-        return { name: "Beggin (Maneskin)", file: "assets/beggin.mp3" };
-      }
-      if (gostaDeMPB) {
-        return { name: "Descobridor dos 7 mares (Tim Maia)", file: "assets/setemares.mp3" };
-      }
-    }
-    if (idade <= 14) {
-      if (gostaDeRap) {
-        return { name: "Diário De Um Detento (Racionais)", file: "assets/diario.mp3" };
-      }
-      if (gostaDeTrap) {
-        return { name: "Clickbait (Veigh)", file: "assets/clickbait.mp3" };
-      }
-      if (gostaDePopInter) {
-        return { name: "Uptown Funk (Mark Ronson)", file: "assets/uptownfunk.mp3" };
-      }
-      if (gostaDePopNac) {
-        return { name: "Meu Talismã (Isa)", file: "assets/talisma.mp3" };
-      }
-      if (gostaDeRock) {
-        return { name: "Enter Sandman (Metallica)", file: "assets/sandman.mp3" };
-      }
-      if (gostaDeMPB) {
-        return { name: "Manias de Você (Rita Lee)", file: "assets/manias.mp3" };
-      }
-    }
+function geraRecomendacao (idade, gostaDeTrap, gostaDePopNac, gostaDeRock) {  
+  if (idade < 18) {
+    if (gostaDeTrap) return "Clickbait (Veigh)";
+    if (gostaDePopNac) return "Meu Talismã (Isa)";
+    if (gostaDeRock) return "Beggin (Maneskin)";
   }
-  if (idade > 18) {
-    if (gostaDeRap) {
-      return { name: "Nego Drama (Racionais)", file: "assets/drama.mp3" };
-    }
-    if (gostaDeTrap) {
-      return { name: "Qual é seu desejo? (Tz da Coro)", file: "assets/desejo.mp3" };
-    }
-    if (gostaDePopInter) {
-      return { name: "Shake it Off (Taylor Swift)", file: "assets/shakeitoff.mp3" };
-    }
-    if (gostaDePopNac) {
-      return { name: "Vafalume (Pollo e Ivo Mozart)", file: "assets/vafalume.mp3" };
-    }
-    if (gostaDeSertanejo) {
-      return { name: "Tijolinho por Tijolinho (Enzo Rabelo)", file: "assets/tijolinho.mp3" };
-    }
-    if (gostaDeRock) {
-      return { name: "Sete vidas (Pitty)", file: "assets/setevias.mp3" };
-    }
-    if (gostaDeMPB) {
-      return { name: "Lança-Perfume (Rita Lee)", file: "assets/lanca.mp3" };
-    }
+  if (idade >= 18) {
+    if (gostaDeTrap) return "Qual é seu desejo? (Tz da Coro)";
+    if (gostaDePopNac) return "Vagalume (Pollo e Ivo Mozar)";
+    if (gostaDeRock) return "Sete vidas (Pitty)";
   }
-  return { name: "", file: "" };
+}
+
+function TocandoMusica(idade, gostaDeTrap, gostaDePopNac, gostaDeRock) {
+  if (idade < 18) {
+    if (gostaDeTrap) return clickbait;
+    if (gostaDePopNac) return talisma;
+    if (gostaDeRock) return beggin;
+  }
+  if (idade >= 18) {
+    if (gostaDeTrap) return desejo;
+    if (gostaDePopNac) return vafalume;
+    if (gostaDeRock) return setevias;
+  }
+  return undefined;
 }
